0caf1ab196dbb4d1f2b49beb0e3aedd95868ee6e
wagihmohamed <wagihm969@gmail.com>
2023-07-23 14:00:22 +0300

    handled login service

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -32,9 +32,6 @@ export const LoginScreen = () => {
   return (
     <div className="w-full h-screen bg-white default">
       <div className="grid grid-cols-12 h-full w-full">
-        <div className="flex bg-white col-span-8 ">
-          <img className="h-full" src={LoginImage} alt="login" />
-        </div>
         <div className="flex flex-col col-span-4 justify-center items-center">
           <h1 className="text-3xl font-bold mb-4">مرحبا بك</h1>
           <p className="text-gray-500 mb-8">
@@ -82,6 +79,9 @@ export const LoginScreen = () => {
               </Button>
             </form>
           </Form>
+        </div>
+        <div className="flex bg-white col-span-8 ">
+          <img className="h-full" src={LoginImage} alt="login" />
         </div>
       </div>
     </div>
