1928635db107c5ff6066e569f3d96298c04ee5f5
wagihmohamed <wagihm969@gmail.com>
2023-07-24 01:44:18 +0300

    fixed svg errors

--- src/components/ui/SideBar/index.tsx
+++ src/components/ui/SideBar/index.tsx
@@ -1,14 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { cn } from "@/lib/utils";
-import { Button, ScrollArea } from "@/components";
+import { Button } from "@/components";
 
-import { Playlist } from "./data";
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  playlists: Playlist[];
-}
-
-export function Sidebar({ className, playlists }: SidebarProps) {
+export function Sidebar({ className }: SidebarProps) {
   return (
     <div className={cn("pb-11 bg-slate-50", className)}>
       <div className="space-y-4 py-4 flex flex-col h-full">
@@ -175,9 +170,9 @@ export function Sidebar({ className, playlists }: SidebarProps) {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="ml-2 h-4 w-4"
             >
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
