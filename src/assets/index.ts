a95f896c4b2474b8f5f9a221b97baaec0a102f33
wagihmohamed <wagihm969@gmail.com>
2023-07-30 12:22:25 +0300

    applying the new theme

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import LoginImage from "./login-image.jpg";
 import ErrorImage from "./error.png";
+import LogoBG from "./logo-bg.jpg";
 
-export { LoginImage, ErrorImage };
+export { LoginImage, ErrorImage, LogoBG };
