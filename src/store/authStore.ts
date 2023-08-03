c3b2b219fb9ae7c974a57e421c859a0ae5e17572
wagihmohamed <wagihm969@gmail.com>
2023-08-04 00:23:32 +0300

    handled login API call.

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,49 +1,98 @@
 import { queryClient, router } from "@/main";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
+import { LoginResponse } from "@/models";
 
-interface AuthStore {
-  id: number;
-  username: string;
-  token: string | null;
-  email: string;
-  setToken: (token: string) => void;
+interface AuthStore extends LoginResponse {
+  setLoginVlaues: (token: LoginResponse) => void;
   logout: () => void;
 }
 
-interface TokenDecoded {
-  username: string;
-  id: number;
-  email: string;
-}
+// interface TokenDecoded {
+//   userId: number;
+//   iat: number;
+//   exp: number;
+// }
 
 const useAuthStore = create<AuthStore>()(
   persist(
     (set) => ({
-      token: null,
-      email: "",
-      id: 0,
-      username: "",
-      setToken: (userToken) => {
-        localStorage.setItem("token", userToken);
-        const decodedToken: TokenDecoded = jwt_decode(userToken);
-        const { email, id, username } = decodedToken;
+      message: "",
+      user: {
+        id: 0,
+        name: "",
+        fcm: "",
+        email: "",
+        phone: "",
+        role: '',
+        super_admin: false,
+        roles: {
+          id: 0,
+          manage_orders: false,
+          manage_products: false,
+          manage_admins: false,
+          manage_deliveries: false,
+          manage_vendors: false,
+          createdAt: "",
+          updatedAt: "",
+          adminId: 0,
+        },
+        token: "",
+      },
+      setLoginVlaues: (LoginResponse: LoginResponse) => {
+        localStorage.setItem("token", LoginResponse.user.token);
         set({
-          email,
-          token: userToken,
-          id,
-          username,
+          message: LoginResponse.message,
+          user: {
+            email: LoginResponse.user.email,
+            fcm: LoginResponse.user.fcm,
+            id: LoginResponse.user.id,
+            name: LoginResponse.user.name,
+            phone: LoginResponse.user.phone,
+            role: LoginResponse.user.role,
+            roles: {
+              adminId: LoginResponse.user.roles.adminId,
+              createdAt: LoginResponse.user.roles.createdAt,
+              id: LoginResponse.user.roles.id,
+              manage_admins: LoginResponse.user.roles.manage_admins,
+              manage_deliveries: LoginResponse.user.roles.manage_deliveries,
+              manage_orders: LoginResponse.user.roles.manage_orders,
+              manage_products: LoginResponse.user.roles.manage_products,
+              manage_vendors: LoginResponse.user.roles.manage_vendors,
+              updatedAt: LoginResponse.user.roles.updatedAt,
+            },
+            super_admin: LoginResponse.user.super_admin,
+            token: LoginResponse.user.token,
+          }
         });
       },
       logout: () => {
-        localStorage.removeItem("token");
         set({
-          token: null,
-          email: "",
-          id: 0,
-          username: "",
+          message: "",
+          user: {
+            id: 0,
+            name: "",
+            fcm: "",
+            email: "",
+            phone: "",
+            role: "",
+            super_admin: false,
+            roles: {
+              id: 0,
+              manage_orders: false,
+              manage_products: false,
+              manage_admins: false,
+              manage_deliveries: false,
+              manage_vendors: false,
+              createdAt: "",
+              updatedAt: "",
+              adminId: 0,
+            },
+            token: "",
+          },
         });
+        localStorage.removeItem("token");
         queryClient.clear();
         router.navigate(`/`);
       },
