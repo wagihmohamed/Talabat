cdcf65cd67dc1662e8a347917620fcdec29c76b5
Wagih mohamed <76626899+wagihmohamed@users.noreply.github.com>
2023-08-15 22:12:37 +0300

    Revert "fixed api issue"

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_BASE_URL || 'http://13.51.235.128:3000/'
+const baseURL = import.meta.env.VITE_BASE_URL
+
+console.log(baseURL);
 
 export const api = axios.create({
   baseURL,
