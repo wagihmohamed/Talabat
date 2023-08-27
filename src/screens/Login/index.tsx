35cf6d44ead2075ec41201b26f7d6a24d8aa988f
wagihmohamed <wagihm969@gmail.com>
2023-08-27 13:29:45 +0300

    added dark mode to login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -33,15 +33,15 @@ export const LoginScreen = () => {
   };
 
   return (
-    <div className="w-full h-screen bg-white default">
+    <div className="w-full h-screen default">
       <div className="grid grid-cols-12 h-full w-full">
         <div className="flex flex-col col-span-12 md:col-span-6 lg:col-span-4 justify-center items-center">
           <h1 className="text-3xl font-bold mb-4">مرحبا بك</h1>
-          <p className="text-gray-500 mb-8">
+          <p className="mb-8">
             سجل الدخول للمتابعة الى لوحة التحكم
           </p>
           {isError && (
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
+            <div className="bg-destructive border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
               <strong className="font-bold">خطأ!</strong>
               <span className="block sm:inline">{error?.message}</span>
             </div>
@@ -83,7 +83,7 @@ export const LoginScreen = () => {
             </form>
           </Form>
         </div>
-        <div className="hidden bg-white sm:hidden md:flex md:col-span-6 lg:col-span-8 ">
+        <div className="hidden sm:hidden md:flex md:col-span-6 lg:col-span-8 ">
           <img
             className="h-full sm:hidden md:block object-cover"
             src={LoginImage}
